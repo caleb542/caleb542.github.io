12a81254dd802e55bae64aeea41632ff1aa8bd9c
Caleb Hamilton <caleb542@users.noreply.github.com>
2016-03-11 11:51:41 -0500

    Update jquerytest.js

--- js/jquerytest.js
+++ js/jquerytest.js
@@ -1,8 +1,16 @@
-var ww = $(window).width();
-if(ww<0){
+function jquerytest(){
+  var ww = $(window).width();
+  if(ww<0){
 
-alert('Window is impossibly small');
+    alert("This window is impossibly small");
 
-}else{
-alert(This is a jquery test.  Would you like your " + ww + " pixel window washed?)
+  }else{
+    alert("This is a jquery test.  Would you like your " + ww + " pixel window washed?");
+  }
+  
+}
+
+$('.site-avatar').click(){
+  
+  jquerytest();
 }

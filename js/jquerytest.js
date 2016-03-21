90dba1d19cc5123e4a34408255229e1abf7ac91b
Caleb Hamilton <caleb542@users.noreply.github.com>
2016-03-21 14:07:26 -0400

    Update jquerytest.js

--- js/jquerytest.js
+++ js/jquerytest.js
@@ -10,14 +10,24 @@
 
 */
 var logo = $('.site-avatar');
+var info = $('.site-info');
+var nav = $('nav');
 function tw(){
-	  TweenMax.to(logo, 2.3, {
+	  TweenMax.to(nav, 2.3, {
 		display:'block',
 		position:'relative',
 		'z-index':4,
-		left:'100%',
-		 ease: Power0.easeNone
+		float:left;
+		ease: Power0.easeNone
 		})
+		TweenMax.to(info,2, {
+		display:'block',
+		position:'relative',
+		'z-index':4,
+		float:right;
+		ease: Power0.easeNone
+		})
+		
 	
 }
 $('.site-info').click( function() {

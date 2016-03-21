154ef1c15ec4667b9dc0a0a6ddf429147bc47958
Caleb Hamilton <caleb542@users.noreply.github.com>
2016-03-21 13:20:57 -0400

    Update jquerytest.js

--- js/jquerytest.js
+++ js/jquerytest.js
@@ -1,16 +1,22 @@
 function jquerytest(){
   var ww = $(window).width();
   if(ww<0){
-
     alert("This window is impossibly small");
-
   }else{
     alert("This is a jquery test.  Would you like your " + ww + " pixel window washed?");
   }
   return ww;
 };
 
+
 $('.site-avatar').click( function() {
-  
-  jquerytest('ww');
+  //jquerytest('ww');
+  TweenMax.to(crosshairs, .5, {
+	
+		//	'background-position-x':coordinateX + "px",
+		//	'background-position-y': coordinateY + "px",
+		 backgroundPosition:coordinateX + "px " + coordinateY + "px",
+			ease:Bounce.easeInOut
+			
+		})
 });

1631cfa61d77d613feac5ae53968a67d37339618
Caleb Hamilton <caleb542@users.noreply.github.com>
2016-03-21 14:34:03 -0400

    Update jquerytest.js

--- js/jquerytest.js
+++ js/jquerytest.js
@@ -14,13 +14,13 @@ var info = $('.site-info');
 var info_h1 = $('.site-info h1');
 var nav = $('nav');
 function tw(){
-	  TweenMax.to(nav, 2.3, {
+/*	  TweenMax.to(nav, 2.3, {
 		display:'block',
 		position:'relative',
 		'z-index':4,
 		float:'left',
 		ease: Power0.easeNone
-		})
+		})*/
 	//use the defaults
 	TweenLite.to(info_h1, 1, {scrambleText:"THIS IS NEW TEXT"}); 
 
